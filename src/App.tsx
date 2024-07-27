e16d9cf4f0cb97872acb10764eb8a18a93b5ce34
joselisboaa <joselisboaa05@gmail.com>
2024-07-27 13:41:06 -0300

    default

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { FormComponent } from "@/components/FormComponent"
 
 function App() {
   return (
     <>
       <h1>Mapa de Acessibilidade</h1>
+      <FormComponent />
     </>
   )
 }
