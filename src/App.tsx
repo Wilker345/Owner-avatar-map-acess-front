25f88d4f7a61708dc653348c5dcbd0ee6b457098
joselisboaa <joselisboaa05@gmail.com>
2024-08-04 22:05:48 -0300

    feat: project config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import './App.css'
-import { FormComponent } from "@/components/FormComponent"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import Login from '@/components/Login';
+import FormComponent from '@/components/FormComponent';
+import queryClient from '@/components/queryClient';
+import Register from '@/components/Register';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <h1>Mapa de Acessibilidade</h1>
-      <FormComponent />
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/form" element={<FormComponent />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
